b4b00a5b06372d69026ecd10f9e096ce78256f1d
Tai <nguyentai3021@gmail.com>
2022-07-04 21:43:02 +0700

    find by slang word and find by Definition

--- src/Main.java
+++ src/Main.java
@@ -9,60 +9,60 @@ public class Main {
         SlangWord slang = new SlangWord();
         System.out.println(slang.ReadSlangwordFile());
         System.out.println(slang.getMap().get("BBC"));
-//        do
-//        {
-//            System.out.println("1. Tim kiem theo slang word");
-//            System.out.println("2. Tim kiem theo dinh nghia");
-//            System.out.println("3. Hien thi history");
-//            System.out.println("4. Them 1 slang word moi");
-//            System.out.println("5. Sua 1 slang word");
-//            System.out.println("6. Xoa 1 slang word");
-//            System.out.println("7. Reset danh sach slang word goc");
-//            System.out.println("8. Random 1 slang word");
-//            System.out.println("9. Do vui voi random slang word");
-//            System.out.println("10. Do vui voi dinh nghia slang word");
-//            System.out.println("____________________________");
-//            System.out.print("Chon chuc nang : ");
-//            chon = in.nextInt();
-//            switch(chon)
-//            {
-//                case 1:
-//
-//                    break;
-//                case 2:
-//
-//                    break;
-//                case 3:
-//
-//                    break;
-//                case 4:
-//
-//                    break;
-//                case 5:
-//
-//                    break;
-//                case 6:
-//
-//                    break;
-//                case 7:
-//
-//                    break;
-//                case 8:
-//
-//                    break;
-//                case 9:
-//
-//                    break;
-//                case 10:
-//
-//                    break;
-//                case 11:
-//                {
-//                    System.out.println("Thoat!");
-//                    System.exit(0);
-//                }
-//            }
-//        }
-//        while(chon!=0);
+        do
+        {
+            System.out.println("1. Tim kiem theo slang word");
+            System.out.println("2. Tim kiem theo dinh nghia");
+            System.out.println("3. Hien thi history");
+            System.out.println("4. Them 1 slang word moi");
+            System.out.println("5. Sua 1 slang word");
+            System.out.println("6. Xoa 1 slang word");
+            System.out.println("7. Reset danh sach slang word goc");
+            System.out.println("8. Random 1 slang word");
+            System.out.println("9. Do vui voi random slang word");
+            System.out.println("10. Do vui voi dinh nghia slang word");
+            System.out.println("____________________________");
+            System.out.print("Chon chuc nang : ");
+            chon = in.nextInt();
+            switch(chon)
+            {
+                case 1:
+                    slang.findBySlangword();
+                    break;
+                case 2:
+                    slang.findByDefinition();2
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+                {
+                    System.out.println("Thoat!");
+                    System.exit(0);
+                }
+            }
+        }
+        while(chon!=0);
     }
 }
--- src/SlangWord.java
+++ src/SlangWord.java
@@ -1,12 +1,13 @@
 import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class SlangWord {
     private HashMap<String, String> map;
     private final Stack<String> historyStack;
-
+    Scanner sc = new Scanner(System.in);
     public SlangWord() {
         this.map = new HashMap<>();
         this.historyStack = new Stack();
@@ -31,12 +32,32 @@ public class SlangWord {
                     }
                     map.put(arrayOfString[0].trim(), arrayOfString[1].trim());
                 }
-//                System.out.println(map);
                 return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
+    }
+    public void findBySlangword() {
+        System.out.println("Nhap slag word can lay : ");
+        String SlagWord = sc.nextLine();
+        historyStack.push(SlagWord);
+
+        if (map.containsKey(SlagWord)) {
+            System.out.println("Slangword tim duoc la : ");
+            System.out.println(map.get(SlagWord));
+        } else {
+            System.out.println("Khong ton tai slangword nay!");
+        }
+    }
+    public void findByDefinition() {
+        System.out.println("Nhap dinh nghia: ");
+        String definition = sc.nextLine();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getValue().toLowerCase().contains(definition.toLowerCase())) {
+                System.out.println(entry.getKey());
+            }
+        }
     }
 }
